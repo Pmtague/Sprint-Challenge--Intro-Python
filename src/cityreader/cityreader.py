2026a36e0c4834bf5af50371e41cd33dc43971d3
Penny Tague <pmtague9@gmail.com>
2020-01-17 09:57:23 -0800

    Completed cityreader

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -2,10 +2,20 @@
 # fields for name, lat and lon (representing latitude and longitude).
 
 
+class City:
+	def __init__(self, name, lat, lon):
+		self.name = name
+		self.lat = lat
+		self.lon = lon
+
+	def __str__(self):
+		return f'City("{self.name}", {self.lat}, {self.lon})'
+
+
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
 #
-# In the body of the `cityreader` function, use Python's built-in "csv" module 
+# In the body of the `cityreader` function, use Python's built-in "csv" module
 # to read this file so that each record is imported into a City instance. Then
 # return the list with all the City instances from the function.
 # Google "python 3 csv" for references and use your Google-fu for other examples.
@@ -16,12 +26,15 @@
 # should not be loaded into a City object.
 cities = []
 
+
 def cityreader(cities=[]):
-  # TODO Implement the functionality to read from the 'cities.csv' file
-  # For each city record, create a new City instance and add it to the 
-  # `cities` list
-    
-    return cities
+	import csv
+	with open('C://Users/pmtag/Documents/Sprint-Challenge--Intro-Python/src/cityreader/cities.csv') as file:
+		csv_reader = csv.DictReader(file)
+		for line in csv_reader:
+			cities.append(City(line['city'], line['lat'], line['lng']))
+
+	return cities
 
 cityreader(cities)
 
